b3e35ca6fc930d9c3c3c3e46f0c9425c8c06b85c
David Frankl <dfrankl@stanford.edu>
2015-06-28 18:24:35 -0700

    pagination

--- javascripts/index.jsx
+++ javascripts/index.jsx
@@ -12,7 +12,9 @@ var Page = React.createClass({
   getInitialState: function () {
     return {
       sortField: this.props.store.sortField,
-      sortMultiplier: this.props.store.sortMultiplier
+      sortMultiplier: this.props.store.sortMultiplier,
+      page: 0,
+      itemsPerPage: 50
     }
   },
 
@@ -33,8 +35,34 @@ var Page = React.createClass({
     });
   },
 
+  first: function () {
+    return this.state.page*this.state.itemsPerPage;
+  },
+
+  last: function () {
+    var last = (this.state.page+1)*this.state.itemsPerPage;
+    if (last > this.props.store.size()) {
+      return this.props.store.size();
+    } else {
+      return last;
+    }
+  },
+
+  getPaginationString: function () {
+    return (this.first()+1) + '-' + this.last() + ' of ' + this.props.store.size();
+  },
+
+  changePage: function (inc) {
+    if (this.state.page + inc >= 0 && this.last() !== this.props.store.size()) {
+      this.setState({
+        page: this.state.page + inc
+      });
+    }
+  },
+
   render: function () {
-    var rows = this.props.store.map(function (row) {
+    var rows = this.props.store.slice(this.first(), this.last())
+                               .map(function (row) {
       return (
         <tr key={row.get('symbol')}>
           <td className='text'>{row.get('name')}</td>
@@ -67,31 +95,38 @@ var Page = React.createClass({
     }
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th className={classNames.name} width='200px' onClick={this.selectColumn.bind(this, 'name')}>
-              {icons.name}
-              Company Name
-            </th>
-            <th className={classNames.symbol} width='100px' onClick={this.selectColumn.bind(this, 'symbol')}>
-              {icons.symbol}
-              Symbol
-            </th>
-            <th className={classNames.market_cap} width='175px' onClick={this.selectColumn.bind(this, 'market_cap')}>
-              {icons.market_cap}
-              Market Cap ($B)
-            </th>
-            <th className={classNames.dividend_yield} width='200px' onClick={this.selectColumn.bind(this, 'dividend_yield')}>
-              {icons.dividend_yield}
-              Dividend Yield (%)
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th className={classNames.name} width='500px' onClick={this.selectColumn.bind(this, 'name')}>
+                {icons.name}
+                Company Name
+              </th>
+              <th className={classNames.symbol} width='100px' onClick={this.selectColumn.bind(this, 'symbol')}>
+                {icons.symbol}
+                Symbol
+              </th>
+              <th className={classNames.market_cap} width='200px' onClick={this.selectColumn.bind(this, 'market_cap')}>
+                {icons.market_cap}
+                Market Cap ($B)
+              </th>
+              <th className={classNames.dividend_yield} width='200px' onClick={this.selectColumn.bind(this, 'dividend_yield')}>
+                {icons.dividend_yield}
+                Dividend Yield (%)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+        <div className='pagination'>
+          {this.getPaginationString()}
+          <i className='fa fa-chevron-left' onClick={this.changePage.bind(this, -1)}/>
+          <i className='fa fa-chevron-right' onClick={this.changePage.bind(this, 1)}/>
+        </div>
+      </div>
     );
   }
 });
